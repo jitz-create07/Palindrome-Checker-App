91947b6686fbc556e523a7e4bdaaeab4772dc9a3
jitz-create07 <jiyaUpadhaya727@gmail.com>
2026-02-27 11:03:55 +0530

    UC4:done

--- src/Palindromecheckerapp.java
+++ src/Palindromecheckerapp.java
@@ -1,18 +1,29 @@
 public class Palindromecheckerapp {
     public static void main(String[] args) {
-        String word = "level";
-        String reversed = "";
+        String word = "radar";
+        char[] chars = word.toCharArray();
+        boolean isPalindrome = true;
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        int start = 0;
+        int end = chars.length - 1;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
         System.out.println("Word to check: " + word);
 
-        if (word.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println("Result: The string is a palindrome.");
         } else {
             System.out.println("Result: The string is not a palindrome.");
         }
     }
+
+
 }
