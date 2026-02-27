dedde6a084b89e320e594d3a5fa4760ca96b0e5d
jitz-create07 <jiyaUpadhaya727@gmail.com>
2026-02-27 09:59:42 +0530

    code changes

--- src/Palindromecheckerapp.java
+++ src/Palindromecheckerapp.java
@@ -1,5 +1,4 @@
 public class Palindromecheckerapp {
-    public static void main(String[] args){
-        System.out.println("Welcome to PAlindrome App");git
+    public static void main(String[] args) {
     }
 }
