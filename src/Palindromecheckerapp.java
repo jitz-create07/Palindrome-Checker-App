f957bdafa8c733c0f9a4b3101e016d2153e52ad0
jitz-create07 <jiyaUpadhaya727@gmail.com>
2026-02-27 10:39:03 +0530

    UC1: code done

--- src/Palindromecheckerapp.java
+++ src/Palindromecheckerapp.java
@@ -1,5 +1,17 @@
 public class Palindromecheckerapp {
+    // Application constants
+    private static final String APP_NAME = "Palindrome Checker Application";
+    private static final String APP_VERSION = "Version 1.0";
+
     public static void main(String[] args){
+        System.out.println("====================================");
+        System.out.println(" Welcome to " + APP_NAME);
+        System.out.println(" " + APP_VERSION);
+        System.out.println("====================================");
+
+        // Flow continues to next use case or exits
+        System.out.println("Application startup complete. Ready for palindrome processing...");
+
 
     }
 }
